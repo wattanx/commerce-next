da5f97c0b8703c1cc975f6bb48b242e1e2701edc
wattanx <wattan.dev@gmail.com>
2024-06-10 10:09:47 +0900

    fix: use view-transition-name unique value

--- app/_lib/Item.tsx
+++ app/_lib/Item.tsx
@@ -2,40 +2,26 @@
 
 import type { ItemResponse } from './types';
 import { Link } from 'next-view-transitions';
-import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 export const Item = ({ item }: { item: ItemResponse }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const activeItemId = searchParams.get('activeItemId');
-
-  const [active, setActive] = useState(item.id === activeItemId);
-
-  useEffect(() => {
-    setActive(false);
-  }, [item.id]);
-
-  const onClick = () => {
-    setActive(true);
-  };
-
   return (
     <Link
       href={`/items/${item.id}/`}
       className="flex min-w-56 flex-col gap-4 px-4 py-2"
-      onClickCapture={onClick}
     >
       <div>
         <img
-          className={active ? 'item-image' : ''}
+          style={{ viewTransitionName: `item-image-${item.id}` }}
           src={item.image_urls[0]}
           width="200"
         />
       </div>
-      <p className={active ? 'item-name' : ''}>{item.name}</p>
-      <p className={active ? 'item-price' : ''}>
+      <p style={{ viewTransitionName: `item-name-${item.id}` }}>{item.name}</p>
+      <p style={{ viewTransitionName: `item-price-${item.id}` }}>
         ¥{item.variations[0].regular_price}
       </p>
     </Link>
--- app/items/[item_id]/page.tsx
+++ app/items/[item_id]/page.tsx
@@ -14,15 +14,23 @@ export default async function ItemDetail({
   return (
     <main className="flex min-h-[calc(100vh-56px)] flex-col items-center pt-8">
       <div>
-        <Link href={`/?activeItemId=${item_id}`}>back</Link>
+        <Link href={`/`}>back</Link>
       </div>
       <div className="mt-10 flex flex-wrap items-center justify-center gap-4">
         <div key={item.id} className="flex min-w-56 flex-col gap-4 px-4 py-2">
           <div>
-            <img className="item-image" src={item.image_urls[0]} width="400" />
+            <img
+              style={{ viewTransitionName: `item-image-${item.id}` }}
+              src={item.image_urls[0]}
+              width="400"
+            />
           </div>
-          <p className="item-name">{item.name}</p>
-          <p className="item-price">¥{item.variations[0].regular_price}</p>
+          <p style={{ viewTransitionName: `item-name-${item.id}` }}>
+            {item.name}
+          </p>
+          <p style={{ viewTransitionName: `item-price-${item.id}` }}>
+            ¥{item.variations[0].regular_price}
+          </p>
           <p>{item.description}</p>
         </div>
       </div>
